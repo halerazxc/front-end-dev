f8f3dad9337f4cb509b6e91edc7f4e0f2a4ecd45
halerazxc <babykinder666@bk.ru>
2025-07-21 20:54:52 +0400

    npm run build (fix errors #3)

--- src/assets/assets.tsx
+++ src/assets/assets.tsx
@@ -7,10 +7,7 @@ import mail from './mail.svg'
 import github from './github.svg'
 import vk from './vk.svg'
 import telegram from './telegram.svg'
-import react from './React.svg'
-import vite from './Vite.svg'
 import tailwind from './Tailwind.svg'
-import shad from './Shadcn.svg'
 import bank from './bank-work.png'
 import hotel from './hotel-work.png'
 import arrowRight from './arrow-right.svg'
@@ -25,10 +22,7 @@ export const assets = {
   github,
   vk,
   telegram,
-  react,
-  vite,
   tailwind,
-  shad,
   bank,
   hotel,
   arrowRight
