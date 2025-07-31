91ebdc35aa9f8e605374dd1230670b39ec7fa441
halerazxc <babykinder666@bk.ru>
2025-07-31 14:10:00 +0400

    added items-center styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className='flex flex-col items-center justify-center py-10'>
         <h1 className='mb-[4px] text-[45px] md:text-[56px] font-medium'><span className='text-gray-500 font-light'>Get</span> in Touch.</h1>
         <p className='mb-[60px] text-[18px] font-light text-white'>So that we can talk more about...</p>
-        <div className='flex flex-row gap-[40px] mb-[30px]'>
+        <div className='flex flex-row gap-[40px] mb-[30px] items-center'>
           <a target="_blank" href="https://t.me/shumelman666"><img width={40} src={assets.telegram} alt="telegram" /></a>
           <a target="_blank" href="https://vk.com/numbthepain666"><img width={40} src={assets.vk} alt="vk" /></a>
           <a target="_blank" href="https://github.com/halerazxc"><img width={40} src={assets.github} alt="github" /></a>
