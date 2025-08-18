fa7ef8cadf284d9a9c3983e0d41fe1204a7ae237
halerazxc <babykinder666@bk.ru>
2025-08-18 11:50:19 +0400

    delete-work

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -30,7 +30,8 @@ const worksData = [
     img: assets.cheese,
     link: "https://halerazxc.github.io/chees-html/",
     reverse: false,
-  }
+  },
+];
 
 const Works = () => {
   const [loading, setLoading] = useState(true);
